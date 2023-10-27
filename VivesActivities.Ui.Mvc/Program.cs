a326474d04a12e007dbd76c5d852ec194bf42dbb
Brent Van Rysselberghe <75610555+brentvr0@users.noreply.github.com>
2023-10-27 12:41:20 +0200

    Initial commit

---
+++ VivesActivities.Ui.Mvc/Program.cs
@@ -0,0 +1,44 @@
+using Microsoft.EntityFrameworkCore;
+using VivesActivities.Ui.Mvc.Core;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+//builder.Services.AddSingleton<VivesActivitiesDbContext>();
+builder.Services.AddDbContext<VivesActivitiesDbContext>(options =>
+{
+    options.UseInMemoryDatabase(nameof(VivesActivitiesDbContext));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+else
+{
+    using var scope = app.Services.CreateScope();
+
+    var database = scope.ServiceProvider.GetRequiredService<VivesActivitiesDbContext>();
+    database.Seed();
+
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
